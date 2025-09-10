bf7aa330e72b0fe0df786145fd31435e0f0ac930
Kallesha G M <kallesh9773@gmail.com>
2025-09-10 23:05:07 +0530

    adding variables

--- variables.tf
+++ variables.tf
@@ -1,7 +1,8 @@
 variable "ami_value" {
     description = "ami-value"
-    default = ""
+    default = "ami-0861f4e788f5069dd"
 }
 variable "instance_type_value" {
     description = "instance_type_value"
+    default = "t2.micro"
 }
